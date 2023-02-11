9074d15daf69eb6334fc9c036ed65136af29c2a3
Taisuke Fukuno <fukuno@jig.jp>
2023-02-12 06:45:12 +0900

    add a fps option

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 フォトグラメトリ「[フォトグラメソッド](https://github.com/code4fukui/photogra-method)」用に、カレントディレクトリの動画ファイル(mov/mp4)をすべて連番静止画(2コマ/秒)変換するプログラム
 
+## usage
+
+```sh
+deno run -A https://code4fukui.github.io/video2jpg/video2jpg.js
+```
+
+fps = 4 (default 2)
+```sh
+deno run -A https://code4fukui.github.io/video2jpg/video2jpg.js 4
+```
+
 ## install
+
+install [Deno](https://deno.land/)
 
 ```sh
 brew install ffmpeg
--- video2jpg.js
+++ video2jpg.js
@@ -1,12 +1,14 @@
 import { dir2array } from "https://js.sabae.cc/dir2array.js";
 
+const fps = Deno.args[0] | 2;
+
 const fns = await dir2array("./");
 let n = 0;
 for (const fn of fns) {
   const fn0 = fn.toLowerCase();
   if (fn0.endsWith(".mov") || fn0.endsWith(".mp4")) {
     console.log(fn);
-    const p = Deno.run({ cmd: ["ffmpeg", "-r", "2", "-qmin", "1", "-q", "1", "-f", "image2", fn + "-%06d.jpg", "-i", fn] });
+    const p = Deno.run({ cmd: ["ffmpeg", "-r", fps, "-qmin", "1", "-q", "1", "-f", "image2", fn + "-%06d.jpg", "-i", fn] });
     await p.status();
   }
 }
